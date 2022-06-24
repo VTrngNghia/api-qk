3e5f84bdbabbf0129485cf6fb53b59d9d57b5c39
NghiaVT <NghiaVT@topcv.vn>
2022-06-24 10:39:18 +0700

    feat: happytime base request interceptor

--- src/instances/happytime.ts
+++ src/instances/happytime.ts
@@ -3,4 +3,9 @@ import axiosBaseConfig from "../config/axios-base-config";
 
 const happyTimeInstance = axios.create(axiosBaseConfig)
 
+happyTimeInstance.interceptors.request.use(config => {
+    console.log("API base request interceptor")
+    return config
+})
+
 export default happyTimeInstance
