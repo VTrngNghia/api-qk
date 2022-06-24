3a1de7777bee4c5a2c43c544e992d9a67dfe42c8
NghiaVT <NghiaVT@topcv.vn>
2022-06-24 10:56:11 +0700

    feat: base interceptor log

--- src/instances/happytime.ts
+++ src/instances/happytime.ts
@@ -3,9 +3,40 @@ import axiosBaseConfig from "../config/axios-base-config";
 
 const happyTimeInstance = axios.create(axiosBaseConfig)
 
-happyTimeInstance.interceptors.request.use(config => {
-    console.log("API base request interceptor")
-    return config
-})
+happyTimeInstance.interceptors.request.use(
+    config => {
+        console.debug(
+            config.method?.toUpperCase(),
+            (config.baseURL || "") + config.url,
+            config,
+        );
+        return config;
+    },
+    Promise.reject
+)
+
+happyTimeInstance.interceptors.response.use(
+    response => {
+        console.debug(
+            response.status,
+            response.config.method?.toUpperCase(),
+            (response.config.baseURL || "") + response.config.url,
+            response.data,
+        );
+        return response;
+    },
+    error => {
+        console.debug(
+            "Base response interceptor error",
+            error.response?.status,
+            error.response?.config?.method?.toUpperCase(),
+            error.response?.config?.url,
+            error,
+            error.response?.data || error.response,
+        );
+
+        return error;
+    }
+)
 
 export default happyTimeInstance
